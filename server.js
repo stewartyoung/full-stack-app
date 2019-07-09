67cef43f2d5715618626929658f6037b7c7656f2
Stewart Young <stewart@dresser.app>
2019-07-09 14:13:28 +0100

    Add router for api, contained in seperate api dir

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import config from './config';
+import apiRouter from './api';
 // import fs from 'fs';
 
 import express from 'express';
@@ -7,6 +8,8 @@ const server = express();
 server.get('/', (req,res) => {
 	res.send('Hello Express');
 });
+
+server.use('/api', apiRouter );
 
 // this line replaces all the lines below by moving about.html from root to public folder, and fs module is no longer required also
 server.use(express.static('public'));
