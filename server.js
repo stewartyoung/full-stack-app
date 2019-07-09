d30b6c9d88b1536c203f8261b76a7f130ce1ec80
Stewart Young <stewart@dresser.app>
2019-07-09 13:48:44 +0100

    add request, response objects to a server

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-import https from 'https';
+import http from 'http';
 
-https.get('https://www.lynda.com', res => {
-	console.log('Response status code: ', res.statusCode);
+const server = http.createServer();
 
-	res.on('data', chunk => {
-		console.log(chunk.toString());
-	});
+server.listen(8080);
+
+server.on('request', (req, res) => {
+	res.write('Hello HTTP\n');
+	setTimeout( () => {
+		res.write('I can stream\n');
+		res.end();
+	}, 3000);
 });
