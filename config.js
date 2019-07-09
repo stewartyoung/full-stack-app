09e32ffc519e74f16102acd9909a9620501c31b5
Stewart Young <stewart@dresser.app>
2019-07-09 13:37:53 +0100

    added import and export functionality

--- config.js
+++ config.js
@@ -1,1 +1,14 @@
-console.log('config');
+//  const as it won't be changed
+const env = process.env;
+
+export const nodeEnv = env.NODE_ENV || 'development'
+
+export const logStars = function(message) {
+	console.info('**********');
+	console.info(message);
+	console.info('**********');
+  };
+
+export default {
+	port: env.PORT || 8080
+};
--- server.js
+++ server.js
@@ -1,2 +1,4 @@
-import "./config";
+import config, { nodeEnv, logStars } from "./config";
 
+console.log(config, nodeEnv);
+logStars('function')
