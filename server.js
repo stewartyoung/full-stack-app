5b740c82fea6aa7bda6769b44220c339b437bcf7
Stewart Young <stewart@dresser.app>
2019-07-09 13:50:10 +0100

    tidied up the basic http server

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 import http from 'http';
 
-const server = http.createServer();
-
-server.listen(8080);
-
-server.on('request', (req, res) => {
+const server = http.createServer((req, res) => {
 	res.write('Hello HTTP\n');
 	setTimeout( () => {
 		res.write('I can stream\n');
 		res.end();
 	}, 3000);
 });
+
+server.listen(8080);
