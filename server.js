9de39bbe62fd95160dffa597755eb1bb4a00cccf
Stewart Young <stewart@dresser.app>
2019-07-09 14:08:28 +0100

    Hooked up public folder for express to use

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import config from './config';
-import fs from 'fs';
+// import fs from 'fs';
 
 import express from 'express';
 const server = express();
@@ -8,11 +8,14 @@ server.get('/', (req,res) => {
 	res.send('Hello Express');
 });
 
-server.get('/about.html', (req,res) => {
-	fs.readFile('./about.html', (err,data) => {
-		res.send(data.toString());
-	});
-});
+// this line replaces all the lines below by moving about.html from root to public folder, and fs module is no longer required also
+server.use(express.static('public'));
+
+// server.get('/about.html', (req,res) => {
+// 	fs.readFile('./about.html', (err,data) => {
+// 		res.send(data.toString());
+// 	});
+// });
 
 server.listen(config.port, () => {
 console.info('Express listening on port ', config.port);
