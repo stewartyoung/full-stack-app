1224513113ab4cb6fd4dbfdad3d08d0773a60a3c
Stewart Young <stewart@dresser.app>
2019-07-09 14:01:35 +0100

    Add server command line console.log for about pg

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-import http from 'http';
+import config from './config';
 
-const server = http.createServer((req, res) => {
-	res.write('Hello HTTP\n');
-	setTimeout( () => {
-		res.write('I can stream\n');
-		res.end();
-	}, 3000);
+import express from 'express';
+const server = express();
+
+server.get('/', (req,res) => {
+	res.send('Hello Express');
 });
 
-server.listen(8080);
+server.get('/about.html', (req,res) => {
+	res.send('The about page');
+});
+
+server.listen(config.port, () => {
+console.info('Express listening on port ', config.port);
+});
