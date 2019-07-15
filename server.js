2593cee622f93177696af2dd0227036145308585
Stewart Young <stewart@dresser.app>
2019-07-15 19:18:09 +0100

    Removed unused code

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import config from './config';
 import apiRouter from './api';
-// import fs from 'fs';
 
 import express from 'express';
 const server = express();
@@ -15,14 +14,8 @@ server.get('/', (req,res) => {
 
 server.use('/api', apiRouter );
 
-// this line replaces all the lines below by moving about.html from root to public folder, and fs module is no longer required also
-server.use(express.static('public'));
 
-// server.get('/about.html', (req,res) => {
-// 	fs.readFile('./about.html', (err,data) => {
-// 		res.send(data.toString());
-// 	});
-// });
+server.use(express.static('public'));
 
 server.listen(config.port, () => {
 console.info('Express listening on port ', config.port);
