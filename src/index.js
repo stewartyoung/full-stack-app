1fd57d6026a191319fb98f2282ed325adf201c43
Stewart Young <stewart@dresser.app>
2019-07-17 19:50:25 +0100

    Using JSX in React

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-	React.createElement('h2', null, 'Hello React'),
+	<h2>Hello React with Jsx {Math.random()}</h2>,
 	document.getElementById('root')	
 );
